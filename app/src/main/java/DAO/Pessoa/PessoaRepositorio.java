1e3663a2e5f4ef1a362c60d66441813cd9fe70e3
sergio <sergio.s.santana@hotmail.com>
2017-09-22 18:50:15 -0300

    nova 22/9

---
+++ app/src/main/java/DAO/Pessoa/PessoaRepositorio.java
@@ -0,0 +1,106 @@
+package DAO.Pessoa;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import Model.Pessoa;
+
+/**
+ * Created by sergi on 21/09/2017.
+ */
+
+public class PessoaRepositorio {
+    private PessoaSQLHelper helper;
+
+    public PessoaRepositorio(Context ctx){
+        helper = new PessoaSQLHelper(ctx);
+    }
+
+    private long inserir(Pessoa pessoa){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+        cv.put(PessoaSQLHelper.COLUMN_NOME, pessoa.getNome());
+        cv.put(PessoaSQLHelper.COLUMN_SOBRENOME, pessoa.getSobrenome());
+        cv.put(PessoaSQLHelper.COLUMN_EMAIL, pessoa.getEmail());
+        cv.put(PessoaSQLHelper.COLUMN_DATA_NASCIMENTO, pessoa.getDataNascimento());
+        cv.put(PessoaSQLHelper.COLUMN_SEXO, pessoa.getSexo());
+        cv.put(PessoaSQLHelper.COLUMN_SENHA, pessoa.getSenha());
+
+        long id = db.insert(PessoaSQLHelper.TABLE_PESSOA, null, cv);
+        if(id != -1){
+            pessoa.setPessoaId(id);
+        }
+        db.close();
+        return id;
+    }
+
+    private int atualizar(Pessoa pessoa){
+        SQLiteDatabase db = helper.getWritableDatabase();
+
+        ContentValues cv = new ContentValues();
+
+        cv.put(PessoaSQLHelper.COLUMN_ID, pessoa.getPessoaId());
+        cv.put(PessoaSQLHelper.COLUMN_NOME, pessoa.getNome());
+        cv.put(PessoaSQLHelper.COLUMN_SOBRENOME, pessoa.getSobrenome());
+        cv.put(PessoaSQLHelper.COLUMN_EMAIL, pessoa.getEmail());
+        cv.put(PessoaSQLHelper.COLUMN_DATA_NASCIMENTO, pessoa.getDataNascimento());
+        cv.put(PessoaSQLHelper.COLUMN_SEXO, pessoa.getSexo());
+        cv.put(PessoaSQLHelper.COLUMN_SENHA, pessoa.getSenha());
+
+        int linhasAfetadas = db.update(PessoaSQLHelper.TABLE_PESSOA, cv,
+                PessoaSQLHelper.COLUMN_ID + " = ?",
+                new String[]{String.valueOf(pessoa.getPessoaId())});
+        db.close();
+        return linhasAfetadas;
+    }
+
+    public void salvar(Pessoa pessoa){
+        if(pessoa.getPessoaId() == 0){
+            inserir(pessoa);
+        }else{
+            atualizar(pessoa);
+        }
+    }
+
+    public int excluir(Pessoa pessoa){
+        SQLiteDatabase db = helper.getWritableDatabase();
+        int linhasAfetadas = db.delete(PessoaSQLHelper.TABLE_PESSOA,
+                PessoaSQLHelper.COLUMN_ID + " = ?",
+                new String[]{String.valueOf(pessoa.getPessoaId())});
+        db.close();
+        return linhasAfetadas;
+    }
+
+    public List<Pessoa> buscarPessoa(String filtro){
+        SQLiteDatabase db = helper.getReadableDatabase();
+
+        String sql = "SELECT * FROM " + PessoaSQLHelper.TABLE_PESSOA;
+        String[] argumentos = null;
+        if(filtro != null){
+            sql += " WHERE " + PessoaSQLHelper.COLUMN_NOME + " LIKE ?";
+            argumentos = new String[]{filtro};
+        }
+        sql += " ORDER BY " + PessoaSQLHelper.COLUMN_NOME;
+        Cursor cursor = db.rawQuery(sql, argumentos);
+        List<Pessoa> pessoas = new ArrayList<Pessoa>();
+        while (cursor.moveToNext()){
+            int id = cursor.getInt(cursor.getColumnIndex(PessoaSQLHelper.COLUMN_ID));
+            String nome = cursor.getString(cursor.getColumnIndex(PessoaSQLHelper.COLUMN_NOME));
+            String sobrenome = cursor.getString(cursor.getColumnIndex(PessoaSQLHelper.COLUMN_SOBRENOME));
+            String email = cursor.getString(cursor.getColumnIndex(PessoaSQLHelper.COLUMN_EMAIL));
+            String dataNacimento = cursor.getString(cursor.getColumnIndex(PessoaSQLHelper.COLUMN_DATA_NASCIMENTO));
+            String sexo = cursor.getString(cursor.getColumnIndex(PessoaSQLHelper.COLUMN_SEXO));
+            String senha = cursor.getString(cursor.getColumnIndex(PessoaSQLHelper.COLUMN_SENHA));
+            Pessoa pessoa = new Pessoa(id, nome, sobrenome, email, dataNacimento, sexo, senha);
+            pessoas.add(pessoa);
+        }
+        cursor.close();
+        db.close();
+        return pessoas;
+    }
+}
---
+++ app/src/main/java/DAO/Pessoa/PessoaSQLHelper.java
@@ -0,0 +1,46 @@
+package DAO.Pessoa;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by sergi on 21/09/2017.
+ */
+
+public class PessoaSQLHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "pessoaDb";
+    private static final int DATABASE_VERSION = 1;
+
+    public static final String TABLE_PESSOA = "pessoa";
+    public static final String COLUMN_ID = "pessoaId";
+    public static final String COLUMN_NOME = "name";
+    public static final String COLUMN_SOBRENOME= "sobrenome";
+    public static final String COLUMN_EMAIL = "email";
+    public static final String COLUMN_DATA_NASCIMENTO= "dataNascimento";
+    public static final String COLUMN_SEXO= "sexo";
+    public static final String COLUMN_SENHA= "senha";
+
+    public PessoaSQLHelper(Context context){
+        super(context, DATABASE_NAME, null, DATABASE_VERSION );
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase){
+        sqLiteDatabase.execSQL(
+                "CREATE TABLE " + TABLE_PESSOA + " (" +
+                        COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                        COLUMN_NOME + " TEXT NOT NULL, " +
+                        COLUMN_SOBRENOME + " TEXT NOT NULL, " +
+                        COLUMN_EMAIL + " TEXT, " +
+                        COLUMN_DATA_NASCIMENTO + " TEXT, " +
+                        COLUMN_SEXO + " TEXT, " +
+                        COLUMN_SENHA + " TEXT)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int NewVersion){
+        //próxima versões
+    }
+
+}
